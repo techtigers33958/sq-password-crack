495af23d4ae6fefcb75bf9d816246bd177512225
techtigers33958 <53841748+techtigers33958@users.noreply.github.com>
2020-03-01 10:21:24 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,37 @@
-function chooseTarget () {
-    Target_Number = Math.randomRange(1, 999)
-    basic.showNumber(Target_Number)
+function release_ball () {
+    pins.servoWritePin(AnalogPin.P4, 45)
+    basic.pause(1000)
+    pins.servoWritePin(AnalogPin.P4, 100)
 }
+input.onButtonPressed(Button.B, function () {
+    reset()
+})
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber != Target_Number) {
         radio.sendString("NO")
+        basic.showIcon(IconNames.SmallSquare)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
     } else {
         radio.sendString("YES")
-        chooseTarget()
+        basic.showIcon(IconNames.Yes)
+        release_ball()
+        reset()
     }
 })
+function reset () {
+    Target_Number = Math.randomRange(1, 999)
+    basic.showIcon(IconNames.Diamond)
+}
+input.onButtonPressed(Button.A, function () {
+    release_ball()
+})
 let Target_Number = 0
-radio.setGroup(1)
-chooseTarget()
+radio.setGroup(40)
+reset()
