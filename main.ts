aaffe73a5c1af0462dfb1f77078624a7c568494c
techtigers33958 <53841748+techtigers33958@users.noreply.github.com>
2020-03-07 10:23:53 -0500

    Comments and organization

--- main.ts
+++ main.ts
@@ -1,11 +1,24 @@
+// The challenges gives user the ball by moving the
+// motor slightly
 function release_ball () {
     pins.servoWritePin(AnalogPin.P4, 45)
     basic.pause(1000)
     pins.servoWritePin(AnalogPin.P4, 100)
 }
+// Manually reset if challenge is disrupted
 input.onButtonPressed(Button.B, function () {
     reset()
 })
+// When random number received
+//
+// 1. Check if received number is the passcode
+//
+//
+// 2. If it is no, No is sent
+//
+//
+// 3. If it is, Yes is send and the ball is released.
+// Also, challenge is reset
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber != passcode) {
         radio.sendString("NO")
@@ -16,10 +29,12 @@ radio.onReceivedNumber(function (receivedNumber) {
         reset()
     }
 })
+// Define reset; different password is set each time
 function reset () {
     passcode = Math.randomRange(1, 999)
     basic.showIcon(IconNames.Diamond)
 }
+// Manually release ball if it gets stuck
 input.onButtonPressed(Button.A, function () {
     release_ball()
 })
